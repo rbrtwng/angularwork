4e395ba3683018b664ebc67f09a2e49e19405658
Robert Wang <rbrtwng.cn@gmail.com>
2016-06-04 17:22:25 +0000

    assignment 3

--- conFusion/app/scripts/controllers.js
+++ conFusion/app/scripts/controllers.js
@@ -1,14 +1,15 @@
-	'use strict';
+'use strict';
 
 angular.module('confusionApp')
 
-        .controller('MenuController', ['$scope', 'manuFactory',function($scope, manuFactory) {
+        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
             
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes = manuFactory.getDishes();
+            $scope.dishes= menuFactory.getDishes();
+
                         
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -53,7 +54,7 @@ angular.module('confusionApp')
                 
                 console.log($scope.feedback);
                 
-                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
                     $scope.invalidChannelSelection = true;
                     console.log('incorrect');
                 }
@@ -67,43 +68,46 @@ angular.module('confusionApp')
             };
         }])
 
-        .controller('DishDetailController', ['$scope', '$stateParams','manuFactory',function($scope, $stateParams, manuFactory) {
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-       
+            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
             
-        $scope.dish = manuFactory.getDish(parseInt($stateParams.id,10));
+            $scope.dish = dish;
             
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
             
-            //Step 1: Create a JavaScript object to hold the comment from the form
-            $scope.comment = {
-              rating: "5",
-              comment: "",
-              author: "",
-              date: ""
-            };
-
+            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            
             $scope.submitComment = function () {
-
-              //Step 2: This is how you record the date
-              //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
-              $scope.comment.date = new Date().toISOString();
-              // Step 3: Push your comment into the dish's comment array
-              $scope.dish.comments.push($scope.comment);
-              //Step 4: reset your form to pristine
-              $scope.commentForm.$setPristine();
-
-              console.log($scope.dish);
-              //Step 5: reset your JavaScript object that holds your comment
-              $scope.comment = {
-                author: "",
-                rating: "5",
-                comment: "",
-                date:""
-              };
+                
+                $scope.mycomment.date = new Date().toISOString();
+                console.log($scope.mycomment);
+                
+                $scope.dish.comments.push($scope.mycomment);
+                
+                $scope.commentForm.$setPristine();
+                
+                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             };
         }])
+
+        // implement the IndexController and About Controller here
+        
+        .controller('IndexController', ['$scope','menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory){
+            
+            $scope.dish = menuFactory.getDish(0);
+            $scope.promo = menuFactory.getPromotion(0);
+            $scope.chef = corporateFactory.getLeader(3);
+            
+        }])
+        
+          .controller('AboutController', ['$scope','corporateFactory', function($scope,corporateFactory){
+              
+              $scope.leaders = corporateFactory.getLeaders();
+            
+        }])
+
 
 ;
